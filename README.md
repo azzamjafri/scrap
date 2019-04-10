bd56b8437988ff0536366ba21cf39181b06bad26
azzamjafri <44396829+azzamjafri@users.noreply.github.com>
2019-04-10 23:01:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+### Learning project
+- just a try !
---
+++ test.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open('test.html') as html_file:
+	soup = BeautifulSoup(html_file, 'lxml')
+
+#print(soup.prettify())
+
+for article in soup.find_all('div', class_='article'):
+	#print(article)
+
+	headline = article.h2.a.text
+	print(headline)
+
+	summary = article.p.text
+	print(summary)
+
+	print()
